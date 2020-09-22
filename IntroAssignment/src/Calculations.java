4c3c1d68f459c4a7429aec21c93f47e66489c47e
dhandkhushi <dhandkhushi07@gmail.com>
2020-09-22 14:44:01 -0700

    yfvtg

--- IntroAssignment/src/Calculations.java
+++ IntroAssignment/src/Calculations.java
@@ -3,26 +3,25 @@ public class Calculations {
     static double a;
     static double b;
     static double c;
-    static Double d;
+    static double d;
 
     static float finalEquation;
     static float finalEquationAnswer1;
     static float finalEquationAnswer2;
 
-    Calculations(double a1, double b1, double c1, Double d1) {
+    Calculations(double a1, double b1, double c1, double d1) {
         this.a = a1;
         this.b = b1;
         this.c = c1;
         this.d = d1;
     }
 
-
     public static void main(String[] args) {
         System.out.println("First Question: ");
         System.out.println(linear(3, 2, 5));
 
-        System.out.println("Second question: ");
-        System.out.println(quadratic(2, 5, -3, -6));
+        System.out.println("Second Question: ");
+        System.out.println(quadratic(2, -5, 3, 6));
 
         System.out.println("Third Question: ");
         System.out.println(distance(4, 9, 56, 4));
@@ -38,28 +37,27 @@ public class Calculations {
     }
 
     //ax^2 + bx + c = d
-   public static double[] quadratic(double a, double b, double c, double d) {
-        double [] answer = new double [3];
-        double discrimininate = getDiscriminate(b, a, c);
-        if (discrimininate < 0){
-            answer[0] = 0;
-            return answer;
+   public static String quadratic(double a, double b, double c, double d) {
+        double subtract = c - (d);
+        double divide = 2 * a;
+        float squareRootEquation = (float) (Math.pow(b, 2) - (4 * a * subtract));
+        if (squareRootEquation < 0 || a == 0) {
+            return "No solutions";
         }
-        else if(discrimininate == 0){
-            answer [0] = 1;
-            double solution = (b*(-1)/2*a);
-            answer[1] = solution;
+        else if (squareRootEquation == 0) {
+            finalEquation = (float) ((-(b) + Math.sqrt(squareRootEquation)) / divide);
+            String answer1 = Float.toString(finalEquation);
+            return answer1;
         }
-        else{
-            answer[0] = 2;
-            double solution = (b*(-1) + (Math.sqrt((b*b) - (4*a*c))))/2*a;
+        else {
+            finalEquationAnswer1 = (float) ((-(b) + Math.sqrt(squareRootEquation)) / divide);
+            finalEquationAnswer2 = (float) ((-(b) - Math.sqrt(squareRootEquation)) / divide);
+            String answer1 = Float.toString(finalEquationAnswer1);
+            String answer2 = Float.toString(finalEquationAnswer2);
+            return answer1 + "  ,  " + answer2;
         }
 
    }
-
-    private static double getDiscriminate(double b, double a, double c) {
-        return (b+b) - (4*a*c);
-    }
 
     //distance between 2 points
     public static double distance(double a, double b, double c, double d) {
@@ -71,6 +69,7 @@ public class Calculations {
         float finalVerticalDistance = (float) Math.pow(y, 2);
 
         finalEquation = (float) Math.sqrt(finalHorizontalDistance + finalVerticalDistance);
+
         return finalEquation;
 
    }
@@ -82,7 +81,9 @@ public class Calculations {
         //vertical distance
         double y = d-b;
         float tempAnswer = (float) Math.atan(x/y);
+
         finalEquation = (float) Math.toDegrees(tempAnswer);
+
         return finalEquation;
     }
 
